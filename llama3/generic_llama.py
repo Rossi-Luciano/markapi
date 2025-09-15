a32aadf8d52c45d3679dc0424d954475fb0997a1
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-09-15 20:28:07 -0300

    Fix __init__

--- llama3/generic_llama.py
+++ llama3/generic_llama.py
@@ -6,8 +6,8 @@ import os
 class GenericLlama:
 
   def __init__(self, messages, response_format, max_tokens=4000, temperature=0.5, top_p=0.5): 
-    self. llm = Llama(model_path = os.path.join(LLAMA_MODEL_DIR, MODEL_LLAMA), n_ctx=4000)
-    self. messages = messages
+    self.llm = Llama(model_path = os.path.join(LLAMA_MODEL_DIR, MODEL_LLAMA), n_ctx=4000)
+    self.messages = messages
     self.response_format = response_format
     self.max_tokens = max_tokens
     self.temperature = temperature
