3f12e9b0fdd62259f4106b06aee1f4960e20c20b
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2026-05-15 08:02:13 -0300

    Upgrade Wagtail 7.4 LTS + Django 6.0

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,6 +15,8 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=[])
 
 # DATABASES
 # ------------------------------------------------------------------------------
