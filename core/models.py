80d0a476d06788cc1f58e6b231bb9c86377d292a
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-06-01 10:28:47 -0300

    black

--- core/models.py
+++ core/models.py
@@ -1,18 +1,19 @@
 import os
-from django.db import models, IntegrityError
-from django.db.models import Case, When, Value, IntegerField
+
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError, models
+from django.db.models import Case, IntegerField, Value, When
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
-from wagtail.search import index
 from wagtailautocomplete.edit_handlers import AutocompletePanel
 
 from . import choices
 from .utils.utils import language_iso
 
 User = get_user_model()
+
 
 class CommonControlField(models.Model):
     """
@@ -78,10 +79,8 @@ class Gender(CommonControlField):
         FieldPanel("gender"),
     ]
 
-
     class Meta:
         unique_together = [("code", "gender")]
-
 
     def __unicode__(self):
         return self.gender or self.code
@@ -225,17 +224,20 @@ class LanguageFallbackManager(models.Manager):
         mission = self.filter(language=language)
         if mission:
             return mission
-        
-        language_order = ['pt', 'es', 'en']
+
+        language_order = ["pt", "es", "en"]
         langs = self.all().values_list("language", flat=True)
         languages = Language.objects.filter(id__in=langs)
-        
-        # Define a ordem baseado na lista language_order
-        order = [When(code2=lang, then=Value(i)) for i, lang in enumerate(language_order)]
-        ordered_languages = languages.annotate(
-            language_order=Case(*order, default=Value(len(language_order)), output_field=IntegerField())
-        ).order_by('language_order')
 
+        # Define a ordem baseado na lista language_order
+        order = [
+            When(code2=lang, then=Value(i)) for i, lang in enumerate(language_order)
+        ]
+        ordered_languages = languages.annotate(
+            language_order=Case(
+                *order, default=Value(len(language_order)), output_field=IntegerField()
+            )
+        ).order_by("language_order")
 
         for lang in ordered_languages:
             mission = self.filter(language=lang)
@@ -258,7 +260,7 @@ class RichTextWithLanguage(models.Model):
         AutocompletePanel("language"),
         FieldPanel("rich_text"),
     ]
-    
+
     objects = LanguageFallbackManager()
 
     class Meta:
@@ -298,7 +300,7 @@ class License(CommonControlField):
     ]
 
     class Meta:
-        unique_together = [("license_type", )]
+        unique_together = [("license_type",)]
         verbose_name = _("License")
         verbose_name_plural = _("Licenses")
         indexes = [
@@ -327,9 +329,7 @@ class License(CommonControlField):
     ):
         if not license_type:
             raise ValueError("License.get requires license_type parameters")
-        filters = dict(
-            license_type__iexact=license_type
-        )
+        filters = dict(license_type__iexact=license_type)
         try:
             return cls.objects.get(**filters)
         except cls.MultipleObjectsReturned:
@@ -369,7 +369,8 @@ class LicenseStatement(CommonControlField):
         Language, on_delete=models.SET_NULL, null=True, blank=True
     )
     license = models.ForeignKey(
-        License, on_delete=models.SET_NULL, null=True, blank=True)
+        License, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     panels = [
         FieldPanel("url"),
@@ -407,7 +408,8 @@ class LicenseStatement(CommonControlField):
             raise ValueError("LicenseStatement.get requires url or license_p")
         try:
             return cls.objects.get(
-                url__iexact=url, license_p__iexact=license_p, language=language)
+                url__iexact=url, license_p__iexact=license_p, language=language
+            )
         except cls.MultipleObjectsReturned:
             return cls.objects.filter(
                 url__iexact=url, license_p__iexact=license_p, language=language
@@ -448,9 +450,7 @@ class LicenseStatement(CommonControlField):
     ):
         try:
             data = dict(
-                url=url,
-                license_p=license_p,
-                language=language and language.code2
+                url=url, license_p=license_p, language=language and language.code2
             )
             try:
                 obj = cls.get(url, license_p, language)
@@ -465,7 +465,9 @@ class LicenseStatement(CommonControlField):
             except cls.DoesNotExist:
                 return cls.create(user, url, license_p, language, license)
         except Exception as e:
-            raise ValueError(f"Unable to create or update LicenseStatement for {data}: {type(e)} {e}")
+            raise ValueError(
+                f"Unable to create or update LicenseStatement for {data}: {type(e)} {e}"
+            )
 
     @staticmethod
     def parse_url(url):
@@ -514,7 +516,7 @@ class FileWithLang(models.Model):
         blank=True,
         on_delete=models.SET_NULL,
         verbose_name=_("File"),
-        help_text='',
+        help_text="",
         related_name="+",
     )
 
@@ -547,12 +549,8 @@ class CoreSyncState(models.Model):
     """
 
     resource = models.CharField(_("Resource"), max_length=50, unique=True)
-    last_updated_at = models.DateTimeField(
-        _("Last updated at"), null=True, blank=True
-    )
-    last_success_at = models.DateTimeField(
-        _("Last success at"), null=True, blank=True
-    )
+    last_updated_at = models.DateTimeField(_("Last updated at"), null=True, blank=True)
+    last_success_at = models.DateTimeField(_("Last success at"), null=True, blank=True)
 
     class Meta:
         verbose_name = _("Core sync state")
@@ -579,7 +577,6 @@ class CoreSyncState(models.Model):
         if self.last_updated_at:
             return self.last_updated_at.date().isoformat()
         return default
-
 
     def update_checkpoint(self, max_updated_at=None):
         """
